10f7ddba60c9b6dd689c1f9e807f79f63790173d
Maryam Fazlali <fazlali1202@gmail.com>
2024-03-25 14:05:31 +0000

    generate URL model

---
+++ db/migrate/20240325135743_create_urls.rb
@@ -0,0 +1,10 @@
+class CreateUrls < ActiveRecord::Migration[7.1]
+  def change
+    create_table :urls do |t|
+      t.string :short_url, :limit => 7
+      t.string :long_url, :limit => 2083
+
+      t.timestamps
+    end
+  end
+end
