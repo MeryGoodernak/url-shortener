82166f47716c210befd83cae8eee39a0e79594de
Maryam Fazlali <fazlali1202@gmail.com>
2024-04-03 12:16:34 +0100

    Add request test for creation of a record

--- spec/requests/urls_spec.rb
+++ spec/requests/urls_spec.rb
@@ -3,7 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Urls' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /' do
+    it 'renders the new template' do
+      get root_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST /urls' do
+    let(:url_params) { { long_url: 'http:://thisisjustfortest.com' } }
+
+    it 'creates a new record to DB' do
+      expect do
+        post urls_path, params: { url: url_params }
+      end.to change(Url, :count).by(1)
+    end
   end
 end
