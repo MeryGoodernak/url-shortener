4b83b105ad833d99f62b677c37a2aed337698c47
Maryam Fazlali <fazlali1202@gmail.com>
2024-04-14 18:23:07 +0100

    Refactoring some repeated lines into methods

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,35 +1,38 @@
 # frozen_string_literal: true
 
 class UrlsController < ApplicationController
-  def show
-    @url = Url.find(params[:id])
-  end
+  before_action :set_url, only: %i[show edit update]
+
+  def show; end
 
   def new
     @url = Url.new
   end
 
-  def edit
-    @url = Url.find(params[:id])
-  end
+  def edit; end
 
   def create
-    short_url = rand(1_111_111...9_999_999).to_s
-    url = Url.new(url_params.merge(short_url:))
+    url = Url.new(url_params.merge(short_url: build_short_url))
     url.save
     redirect_to url_path(url)
   end
 
   def update
-    short_url = rand(1_111_111...9_999_999).to_s
-    url = Url.find(params[:id])
-    url.update(url_params.merge(short_url:))
-    redirect_to url_path(url)
+    @url.update(url_params.merge(short_url: build_short_url))
+    redirect_to url_path(@url)
   end
 
   private
 
+  def set_url
+    @url = Url.find(params[:id])
+  end
+
   def url_params
     params.require(:url).permit(:long_url)
+  end
+
+  def build_short_url
+    rand(1_111_111...9_999_999).to_s
   end
 end
